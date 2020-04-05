6042707360426e56cebfe8941029497ca134d791
Mircea Dobrin <mircea.dobrin@netgas.ro>
2020-04-05 16:12:11 +0300

    test unit example, well not so great, but it's an example

--- app/tests/GameTest.php
+++ app/tests/GameTest.php
@@ -1,27 +1,38 @@
 <?php
 
+use app\models\Beast;
 use app\models\Game;
 use app\models\Hero;
+use app\models\Round;
 
 final class GameTest extends \Codeception\Test\Unit
 {
     public function testHeroInit(): void
     {
-        $author = new Hero();
-        $load = $author->load([
-            'token' => 'test',
-            'last_name' => 'test',
-            'phone' => '080808',
-            'email' => 'test@test',
-            'birthday' => '2020-01-01',
-        ]);
+        $hero = Hero::init();
+        
+        $this->assertInstanceOf(Hero::class, $hero);
+        $beast = Beast::init();
+        
+        $this->assertInstanceOf(Beast::class, $beast);
+        
+        $game = Game::init($hero, $beast);
+        
+        $this->assertInstanceOf(Game::class, $game);
+        
+        $round = Round::newRound($game);
 
-        $this->assertIsBool($load);
+        $this->assertInstanceOf(Round::class, $round);
+        
+        $rounds = $game->rounds;
+        $this->assertIsArray($rounds);
+        
+        if ($hero->speed > $beast->speed) {
+            $hero = json_decode($round->hero_stats, true);
+            $this->assertEquals(1, $hero['hasToAttack']);
+            $bstats = json_decode($round->beast_stats, true);
+            $this->assertGreaterThan($beast->health, $bstats['health']);
 
-        $this->assertEquals('test', $author->first_name);
-
-        $save = $author->save();
-
-        $this->assertTrue($save);
+        }
     }
 }
